1c8f2a2d4b8da40bdc5031980bce8c187104d783
rosy castilllo <rosyycastillo23@gmail.com>
2016-10-06 15:11:21 -0700

    hi

---
+++ app/src/main/java/com/example/student/areyoubored/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.student.areyoubored;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView answerText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        answerText = (TextView) findViewById(R.id.answerText);
+        answerText.setText("Ughh");
+    }
+}
---
+++ app/src/main/java/com/example/student/areyoubored/Predictions.java
@@ -0,0 +1,15 @@
+package com.example.student.areyoubored;
+
+
+
+
+public class Predictions {
+
+    public static Predictions predictions;
+
+    private Predictions (){
+
+
+    }
+
+}
